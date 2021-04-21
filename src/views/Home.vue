36ce014d377f86e15e80d63ea3b58d9a1d05c327
MangGuowjh <1678395645@qq.com>
2021-04-21 09:00:01 +0800

    4.21封装axios

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <!-- <van-button type="primary">按钮</van-button> -->
+    <div>邓琳</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
+
+<style lang="less" scoped>
+.home {
+  div {
+    background: grey;
+    width: 150px;
+    height: 150px;
+    font-size: 18px;
+  }
+}
+</style>
