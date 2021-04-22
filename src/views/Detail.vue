c9fa80f11b87e1b7bd76ac2f48460e6bb647e0c6
MangGuowjh <1678395645@qq.com>
2021-04-22 14:21:57 +0800

    提取公用头部组件

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,12 +1,17 @@
 <template>  
   <div class="detail">    
-    <div>详情页面</div>  
+    <!-- <div>详情页面</div>   -->
+    <s-header :title="'详情'"></s-header>
   </div>
 </template>
 
 <script>
+import sHeader from "@/components/SimpleHeader";
 export default {  
   name: 'detail',
+  components: {    
+    sHeader,  
+  },
 };
 </script>
 
